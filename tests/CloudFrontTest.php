4a62a45580d950d5f9f4d6d8c7671562abd0b17c
Glenn Michael Torregosa <gtorregosa@gmail.com>
2021-03-21 09:15:54 +0800

    chore: add tests & github actions

--- tests/CloudFrontTest.php
+++ tests/CloudFrontTest.php
@@ -32,6 +32,8 @@
 //     }
 // }
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Meema\CloudFront\Facades\CloudFront;
 
 uses(Meema\CloudFront\Tests\CloudFrontTestCase::class);
@@ -48,7 +50,58 @@ it('can fetch cloudfront client', function () {
 });
 
 it('can invalidate image', function () {
-    $paths = '/glenn/1/2/butterfly.jpg';
+    $domainDevCdn = config('cloudfront.distribution_url');
 
-    CloudFront::invalidate($paths, config('cloudfront.distribution_ops_id'));
+    $path = '/glenn/1/3/people.jpg';
+
+    $urlDevCdn = "{$domainDevCdn}{$path}";
+
+    Http::get($urlDevCdn)->body();
+
+    $finalPath = '/glenn/1/3/people.jp*';
+
+    $response = CloudFront::invalidate($finalPath, config('cloudfront.distribution_id'));
+
+    $this->assertEquals($response['@metadata']['statusCode'], 201);
+});
+
+it('can invalidate everything', function () {
+    $domainDevCdn = config('cloudfront.distribution_url');
+
+    $path = '/glenn/1/3/people.jpg';
+
+    $urlDevCdn = "{$domainDevCdn}{$path}";
+
+    Http::get($urlDevCdn)->body();
+
+    $response = CloudFront::reset();
+
+    $this->assertEquals($response['@metadata']['statusCode'], 201);
+});
+
+it('can get invalidation string', function () {
+    $domainDevCdn = config('cloudfront.distribution_url');
+
+    $path = '/glenn/1/3/people.jpg';
+
+    $urlDevCdn = "{$domainDevCdn}{$path}";
+
+    Http::get($urlDevCdn)->body();
+
+    $finalPath = '/glenn/1/3/people.jp*';
+
+    $result = CloudFront::invalidate($finalPath, config('cloudfront.distribution_id'));
+
+    $response = CloudFront::getInvalidation($result['Invalidation']['Id']);
+
+    $status = Str::contains($response, "The status for the invalidation with the ID of {$result['Invalidation']['Id']} is InProgress");
+
+    $this->assertTrue($status);
+});
+
+it('can list invalidations processes', function () {
+    $response = CloudFront::listInvalidations(config('cloudfront.distribution_id'));
+
+    $this->assertTrue(is_array($response));
+    $this->assertTrue(count($response) > 0);
 });
